970db13226bed0b05aa775c6e356d288c42f73f2
Sahil Mishra <slymishra@gmail.com>
2024-12-11 23:33:10 -0500

    added keymaps, git plugin, cleanup folder

--- nvim/init.lua
+++ nvim/init.lua
@@ -353,6 +353,7 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+      vim.keymap.set('n', '<leader>sc', require('telescope.builtin').colorscheme, { desc = '[S]earch [C]olorschemes' })
 
       -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set('n', '<leader>/', function()
--- nvim/lua/sahilkm/keymaps.lua
+++ nvim/lua/sahilkm/keymaps.lua
@@ -2,14 +2,15 @@ local M = {}
 
 M.setup = function()
   vim.keymap.set('n', ';', '<cmd>Telescope commands<CR>', { desc = 'Search Commands' })
+  -- [E]dit
   vim.keymap.set('n', '<leader>ee', '<cmd>Oil<CR>', { desc = '[E]xplore' })
+  vim.keymap.set('n', '<leader>eg', '<cmd>Neogit<CR>', { desc = '[E]dit [G]it' })
 
   -- [S]earch
-  vim.keymap.set('n', '<leader>sc', builtin.colorscheme, { desc = '[S]earch [C]olorschemes' })
 
   -- [T]oggle
   vim.keymap.set('n', '<leader>tr', function()
-    vim.opt.relativenumber = not vim.opt.relativenumber
+    vim.opt.relativenumber = not vim.opt.rnu
   end, { desc = '[T]oggle [R]elative Line Numbers' })
 end
 
