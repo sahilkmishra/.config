e63a144568636ff1431fd63498fd5fe589111775
Sahil Mishra <slymishra@gmail.com>
2024-12-20 11:24:01 -0500

    added autocommand to readd relative line numbers when i open a buffer

--- nvim/lua/sahilkm/terminal.lua
+++ nvim/lua/sahilkm/terminal.lua
@@ -9,7 +9,15 @@ M.setup = function()
       vim.opt.relativenumber = false
     end,
   })
-end
 
+  vim.api.nvim_create_autocmd('BufReadPre', {
+
+    group = vim.api.nvim_create_augroup('reset-term-lineconfig', { clear = true }),
+    callback = function()
+      vim.opt.number = true
+      vim.opt.relativenumber = true
+    end,
+  })
+end
 
 return M
