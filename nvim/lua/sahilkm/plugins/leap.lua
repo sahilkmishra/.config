ab5765b029886fd3bc87ed4d42ef7e1992f88cbb
Sahil Mishra <slymishra@gmail.com>
2024-12-30 22:06:20 -0500

    Update leap.lua

--- nvim/lua/sahilkm/plugins/leap.lua
+++ nvim/lua/sahilkm/plugins/leap.lua
@@ -1,15 +1,3 @@
 return {
-  {
-    'smoka7/hop.nvim',
-    version = '*',
-    opts = {
-      keys = 'etovxqpdygfblzhckisuran',
-    },
-    config = function()
-      require('hop').setup { keys = 'etovxqpdygfblzhckisuran' }
-      vim.keymap.set('n', '<leader><leader>', '<cmd>HopWord<CR>', { desc = 'Hop Word' })
-      vim.keymap.set('v', '<leader><leader>', '<cmd>HopWord<CR>', { desc = 'Hop Word' })
-      --vim.keymap.set('n', '<S-leader><S-leader>', '<Plug>(leap-backward)', { desc = 'Leap' })
-    end,
-  },
+  --{ 'smoka7/hop.nvim', version = '*', opts = { keys = 'etovxqpdygfblzhckisuran', }, config = function() require('hop').setup { keys = 'etovxqpdygfblzhckisuran' } vim.keymap.set('n', '<leader><leader>', '<cmd>HopWord<CR>', { desc = 'Hop Word' }) vim.keymap.set('v', '<leader><leader>', '<cmd>HopWord<CR>', { desc = 'Hop Word' }) --vim.keymap.set('n', '<S-leader><S-leader>', '<Plug>(leap-backward)', { desc = 'Leap' }) end, },
 }
