7c2479c616daa3e3a7914e00e18248cac64d34c7
Sahil Mishra <slymishra@gmail.com>
2025-01-11 12:48:18 -0500

    added browser bookmarks, added todo plugin, nix helper, new keymaps for the previouS

---
+++ nvim/lua/sahilkm/plugins/telescope_plugins.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    'dhruvmanila/browser-bookmarks.nvim',
+    version = '*',
+    -- Only required to override the default options
+    opts = {
+      -- Override default configuration values
+      selected_browser = 'chrome',
+      url_open_command = 'xdg-open',
+    },
+    dependencies = {
+      --   -- Only if your selected browser is Firefox, Waterfox or buku
+      --   'kkharji/sqlite.lua',
+      --
+      --   -- Only if you're using the Telescope extension
+      'nvim-telescope/telescope.nvim',
+    },
+  },
+}
