301fd5ce4728a0a1c7b228f19cc0fccab7660c1c
Sahil Mishra <slymishra@gmail.com>
2025-02-13 19:16:23 -0500

    added filetype go and lua to after/ftplugin

---
+++ nvim/after/ftplugin/lua.lua
@@ -0,0 +1,6 @@
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.g.colorscheme = 'peachpuff'
+vim.keymap.set('n', '<space>td', function()
+  print 'Hello World'
+end, { buffer = 0 })
--- nvim/init.lua
+++ nvim/init.lua
@@ -3,6 +3,7 @@
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
+vim.g.filetype = true
 
 require('sahilkm.keymaps').setup()
 require('sahilkm.terminal').setup()
