2ebfac1aca8323fed009aa49d3923b070cd0a5c8
Sahil Mishra <slymishra@gmail.com>
2025-03-02 17:43:51 -0500

    added codegemma

--- nvim/lua/sahilkm/plugins/llm_gen.lua
+++ nvim/lua/sahilkm/plugins/llm_gen.lua
@@ -3,7 +3,7 @@ return {
   {
     'David-Kunz/gen.nvim',
     opts = {
-      model = 'deepseek-r1', -- The default model to use.
+      model = 'codegemma', -- The default model to use.
       -- model = 'llama3.2:3b', -- The default model to use.
       quit_map = 'q', -- set keymap to close the response window
       retry_map = '<c-r>', -- set keymap to re-send the current prompt
