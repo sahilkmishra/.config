5f40f1e9fc5909dc14911aac5e3495cc4987cee2
Sahil Mishra <slymishra@gmail.com>
2025-04-08 18:38:27 -0400

    added utils

--- nvim/after/ftplugin/norg.lua
+++ nvim/after/ftplugin/norg.lua
@@ -1,2 +1,7 @@
 vim.api.nvim_set_keymap('i', '<M-C-d>', '<Plug>(neorg.promo.demote)', { noremap = true })
 vim.api.nvim_set_keymap('i', '<M-C-t>', '<Plug>(neorg.promo.promote)', { noremap = true })
+
+local set = vim.opt_local
+
+-- TODO: unset line numbers
+require('snacks.toggle').line_number()
