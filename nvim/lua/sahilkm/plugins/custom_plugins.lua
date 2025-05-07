cbfe3ca29de9f109fdc65185e6fcb06b2f0f5d0e
Sahil Mishra <slymishra@gmail.com>
2025-05-07 16:02:42 -0400

    changed lazydev source

--- nvim/lua/sahilkm/plugins/custom_plugins.lua
+++
@@ -1,9 +0,0 @@
-return {
-  {
-    dir = '~/plugins/path_picker/',
-
-    dependencies = {
-      { 'nvim-telescope/telescope.nvim', lazy = true },
-    },
-  },
-}
--- nvim/lua/sahilkm/plugins/lazydev.lua
+++ nvim/lua/sahilkm/plugins/lazydev.lua
@@ -10,16 +10,17 @@ return {
       },
     },
   },
-  --[[ { -- optional cmp completion source for require statements and module annotations
-    "hrsh7th/nvim-cmp",
+  { -- optional cmp completion source for require statements and module annotations
+    'hrsh7th/nvim-cmp',
     opts = function(_, opts)
       opts.sources = opts.sources or {}
       table.insert(opts.sources, {
-        name = "lazydev",
+        name = 'lazydev',
         group_index = 0, -- set group index to 0 to skip loading LuaLS completions
       })
     end,
   },
+  --[[
   { -- optional blink completion source for require statements and module annotations
     "saghen/blink.cmp",
     opts = {
