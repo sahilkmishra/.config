b21168ae6a672c21a9730a645705779b5b92d8a8
Sahil Mishra <slymishra@gmail.com>
2025-05-07 23:12:11 -0400

    added atkinson as ghostty font

--- nvim/lua/sahilkm/plugins/custom_plugins.lua
+++ nvim/lua/sahilkm/plugins/custom_plugins.lua
@@ -1,9 +1,16 @@
 return {
   {
-    dir = '~/plugins/path_picker/',
+    dir = '~/plugins/swap_menu/',
 
     dependencies = {
       { 'nvim-telescope/telescope.nvim', lazy = true },
     },
   },
+  --[[ {
+     dir = '~/plugins/path_picker/',
+
+    dependencies = {
+      { 'nvim-telescope/telescope.nvim', lazy = true },
+    },
+  }, ]]
 }
