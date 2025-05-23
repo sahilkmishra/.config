6aeaa4dcfb80a0f79ef909f36cd87eb22a7699e9
Sahil Mishra <slymishra@gmail.com>
2025-05-23 15:45:33 -0400

    mac changes

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,7 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+eval "$(/opt/homebrew/bin/brew shellenv)"
--- nvim/lua/sahilkm/plugins/builtin/mason_lsp.lua
+++ nvim/lua/sahilkm/plugins/builtin/mason_lsp.lua
@@ -13,7 +13,6 @@ return {
 
       -- `neodev` configures Lua LSP for your Neovim config, runtime and plugins
       -- used for completion, annotations and signatures of Neovim apis
-      { 'folke/neodev.nvim', opts = {} },
     },
     opts = {
       inlay_hints = { enabled = true },
